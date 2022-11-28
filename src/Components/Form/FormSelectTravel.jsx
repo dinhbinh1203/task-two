3c1569f7f3d0791c3bb753cd5516d32bded3eb50
Đinh Thị Thanh Bình <105594732+dinhbinh1203@users.noreply.github.com>
2022-11-28 14:06:56 +0700

    Update

--- src/Components/Form/FormSelectTravel.jsx
+++ src/Components/Form/FormSelectTravel.jsx
@@ -4,7 +4,8 @@ import cities from 'hanhchinhvn/dist/tinh_tp.json';
 import districts from 'hanhchinhvn/dist/quan_huyen.json';
 import wards from 'hanhchinhvn/dist/xa_phuong.json';
 import { MinusCircleOutlined } from '@ant-design/icons';
-import { Form, Select, Row, Col } from 'antd';
+import { Form, Select, Row, Col, Input } from 'antd';
+import Item from 'antd/lib/list/Item';
 
 const { Option } = Select;
 
@@ -114,113 +115,129 @@ const FormSelectTravel = ({ name, form, remove, fields, isListField }) => {
   };
 
   return (
-    <Row gutter={16}>
-      <Col span={7}>
-        <Form.Item
-          isListField={isListField}
-          name={[name, 'city']}
-          rules={[
-            {
-              required: true,
-              message: 'Vui lòng nhập tỉnh/thành phố',
-            },
-          ]}
-        >
-          <Select
-            justify="start"
-            align="start"
-            showSearch
-            filterOption={filterOption}
-            placeholder="Tỉnh/Thành phố"
-            onChange={(value, option) => handleCityChange(value, option, name)}
-          >
-            {Object.values(cities).map((item, index) => (
-              <Option value={item.name} key={index} compare={item.code}>
-                {item.name}
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
-      </Col>
-
-      <Col span={7}>
-        <Form.Item
-          isListField={isListField}
-          dependencies={['city']}
-          name={[name, 'district']}
-          rules={[
-            {
-              required: true,
-              message: 'Vui lòng nhập quận/huyện',
-            },
-          ]}
-        >
-          <Select
-            justify="start"
-            align="start"
-            showSearch
-            filterOption={filterOption}
-            placeholder="Quận/Huyện"
-            onChange={(value, option) =>
-              handleDistrictChange(value, option, name)
-            }
-            disabled={!form.getFieldValue(['travel', name, 'city'])}
-          >
-            {listDistrict !== undefined &&
-              listDistrict.map((item, index) => (
-                <Option value={item.name} key={index} compare={item.code}>
-                  {item.name}
-                </Option>
-              ))}
-          </Select>
-        </Form.Item>
-      </Col>
-
-      <Col span={7}>
-        <Form.Item
-          isListField={isListField}
-          dependencies={['district']}
-          name={[name, 'ward']}
-          rules={[
-            {
-              required: true,
-              message: 'Vui lòng nhập phường xã',
-            },
-            () => ({
-              validator(_, value) {
-                if (checkAddressExactly(name)) {
-                  return Promise.resolve();
+    <Form.Item>
+      <Input.Group>
+        <Row gutter={16}>
+          <Col span={7}>
+            <Form.Item
+              noStyle
+              isListField={isListField}
+              name={[name, 'city']}
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập tỉnh/thành phố',
+                },
+              ]}
+            >
+              <Select
+                className="w-full"
+                justify="start"
+                align="start"
+                showSearch
+                filterOption={filterOption}
+                placeholder="Tỉnh/Thành phố"
+                onChange={(value, option) =>
+                  handleCityChange(value, option, name)
                 }
-                return Promise.reject(
-                  new Error('Địa chỉ không được giống nhau'),
-                );
-              },
-            }),
-          ]}
-        >
-          <Select
-            justify="start"
-            align="start"
-            showSearch
-            filterOption={filterOption}
-            placeholder="Phường/xã"
-            disabled={!form.getFieldValue(['travel', name, 'district'])}
-            onChange={(value, option) => handleWardChange(value, option, name)}
-          >
-            {listWard !== undefined &&
-              listWard.map((item) => (
-                <Option value={item.name} key={item.code}>
-                  {item.name}
-                </Option>
-              ))}
-          </Select>
-        </Form.Item>
-      </Col>
+              >
+                {Object.values(cities).map((item, index) => (
+                  <Option value={item.name} key={index} compare={item.code}>
+                    {item.name}
+                  </Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Col>
 
-      <Col span={3}>
-        {fields.length > 1 && <MinusCircleOutlined onClick={handleRemove} />}
-      </Col>
-    </Row>
+          <Col span={7}>
+            <Form.Item
+              noStyle
+              isListField={isListField}
+              dependencies={['city']}
+              name={[name, 'district']}
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập quận/huyện',
+                },
+              ]}
+            >
+              <Select
+                className="w-full"
+                justify="start"
+                align="start"
+                showSearch
+                filterOption={filterOption}
+                placeholder="Quận/Huyện"
+                onChange={(value, option) =>
+                  handleDistrictChange(value, option, name)
+                }
+                disabled={!form.getFieldValue(['travel', name, 'city'])}
+              >
+                {listDistrict !== undefined &&
+                  listDistrict.map((item, index) => (
+                    <Option value={item.name} key={index} compare={item.code}>
+                      {item.name}
+                    </Option>
+                  ))}
+              </Select>
+            </Form.Item>
+          </Col>
+
+          <Col span={7}>
+            <Form.Item
+              noStyle
+              isListField={isListField}
+              dependencies={['district']}
+              name={[name, 'ward']}
+              rules={[
+                {
+                  required: true,
+                  message: 'Vui lòng nhập phường xã',
+                },
+                () => ({
+                  validator(_, value) {
+                    if (checkAddressExactly(name)) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error('Địa chỉ không được giống nhau'),
+                    );
+                  },
+                }),
+              ]}
+            >
+              <Select
+                className="w-full"
+                justify="start"
+                align="start"
+                showSearch
+                filterOption={filterOption}
+                placeholder="Phường/xã"
+                disabled={!form.getFieldValue(['travel', name, 'district'])}
+                onChange={(value, option) =>
+                  handleWardChange(value, option, name)
+                }
+              >
+                {listWard !== undefined &&
+                  listWard.map((item) => (
+                    <Option value={item.name} key={item.code}>
+                      {item.name}
+                    </Option>
+                  ))}
+              </Select>
+            </Form.Item>
+          </Col>
+
+          <Col span={3}>
+            {fields.length > 1 && (
+              <MinusCircleOutlined onClick={handleRemove} />
+            )}
+          </Col>
+        </Row>
+      </Input.Group>
+    </Form.Item>
   );
 };
 
